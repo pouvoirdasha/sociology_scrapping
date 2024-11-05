292f1dd74252f8735a9578eaa77207fc676792de
Leo le Douarec <leo.alex@le.douar.ec>
2024-11-05 13:32:05 +0100

    put main code in test mode (to avoid exploding API quota)

--- main.py
+++ main.py
@@ -15,23 +15,23 @@ def main():
 
     out = True
     out_filename= f"YouTube-comments_{date.day}-{date.month}-{date.year}_{date.hour}h{date.minute}.csv"
-    yt_ids_to_scrape = [
-        "Js4qqwdjA9M", #549 
-        "fxv-YhT3Ixc", #165
-        "EWUK5A-3LSI", #862
-        "y4BKfR5t1l8", #427
-        "8zPor9l9SwI", #11 838 #WILL CAP THE COMMENTS LIMIT !
-        "w1u9IgepfAU", #393
-        "YF7Hs4GeJkQ", #1 700
-        "1EuF7yfoyDE"  #2 372
-    ]
+    # yt_ids_to_scrape = [
+    #     "Js4qqwdjA9M", #549 
+    #     "fxv-YhT3Ixc", #165
+    #     "EWUK5A-3LSI", #862
+    #     "y4BKfR5t1l8", #427
+    #     "8zPor9l9SwI", #11 838 #WILL CAP THE COMMENTS LIMIT !
+    #     "w1u9IgepfAU", #393
+    #     "YF7Hs4GeJkQ", #1 700
+    #     "1EuF7yfoyDE"  #2 372
+    # ]
 
-    #yt_ids_to_scrape=["Awfy38D90gk"] #for debugging
+    yt_ids_to_scrape=["Awfy38D90gk"] #for debugging/test
 
 
-    #comments = scrape_youtube_videos(yt_ids_to_scrape,out_filename,out)
+    comments = scrape_youtube_videos(yt_ids_to_scrape,out_filename,out)
 
-    # print(f"Actual Number of YouTube comments: {len(comments)}. | Expected number of YouTube comments= {549+165+862+427+393+1700+2372+11838}") #+11838 avec la grosse vidéo.
+    print(f"Actual Number of YouTube comments: {len(comments)}. | Expected number of YouTube comments= {549+165+862+427+393+1700+2372+11838}") #+11838 avec la grosse vidéo.
 
 
     ##############################################################################################################################################################################
